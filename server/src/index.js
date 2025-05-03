a08883a79f5a6c2f018c7a729b8dbd15e29e0dbb
bakoji <mustafabakoji@gmail.com>
2025-05-03 21:50:46 +0100

    deployable

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import { DB_CONNECTION } from "../db/DB_CONNECTION.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import path from "path"
 import { indexRouter } from "../routes/index.js"
 import cloudinary from "cloudinary"
 
@@ -10,15 +11,16 @@ import cloudinary from "cloudinary"
 const app = express()
 dotenv.config()
 app.use(express.json())
-
+app.use(cookieParser())
 
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 })
+const _dirname = path.resolve()
+app.use(express.static(path.join(_dirname, "/client/dist")))
 
-app.use(cookieParser())
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
@@ -30,7 +32,9 @@ DB_CONNECTION()
 
 app.use("/api", indexRouter)
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "client", "dist", "index.html"))
+})
 
 app.listen(4000, () => {
     console.log("server started at port 4000");
